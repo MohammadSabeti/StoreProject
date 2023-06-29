b94ea9733e6a41b21c880c2dd27cd91d458edf1e
Mr S-W-G-D <Mohammadi.tik@gmail.com>
2023-06-29 23:04:33 +0330

    register CustomerAdmin Model.

--- Store/admin.py
+++ Store/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from Store.models import *
 from Store.models import OrderApp
 
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'gender', 'mobile', 'get_balance_display')
+    list_filter = ('address', 'gender')
+    search_fields = ('user__username',)
+    ordering = ['pk']
+
+
+admin.site.register(Customer, CustomerAdmin)
 admin.site.register(OrderApp)
 admin.site.register(Payment)
