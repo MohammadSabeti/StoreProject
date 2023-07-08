0ca17c42d717b66d05ea09a24f607ac6b591e73c
Mr S-W-G-D <Mohammadi.tik@gmail.com>
2023-07-08 12:39:39 +0330

    add orderapp_list to views.

--- Store/views.py
+++ Store/views.py
@@ -21,7 +21,6 @@ def product_list(request):
     return render(request, 'store/product_list.html', context)
 
 
-
 def product_details(request, product_id):
     product = Product.objects.get(pk=product_id)
 
@@ -30,12 +29,11 @@ def product_details(request, product_id):
         'date_now': datetime.now()
     }
 
-
     if request.method == 'POST':
         try:
             sell_count = int(request.POST['sell_count'])
             assert product.ProductStatus == product.Available, 'موجودی کالا در انبار به اتمام رسیده است'
-            assert product.ProductStock >= sell_count , 'با توجه به موجودی این کالا در انبار در حال حاضر متاسفانه نمی توانیم به این تعداد سفارش پاسخگو باشیم.'
+            assert product.ProductStock >= sell_count, 'با توجه به موجودی این کالا در انبار در حال حاضر متاسفانه نمی توانیم به این تعداد سفارش پاسخگو باشیم.'
             price = product.ProductPrice * sell_count
             assert request.user.customer.spend(price), 'اعتبار شما برای ثبت سفارش کافی نیست.'
             product.reserve_stock(sell_count)
@@ -45,4 +43,3 @@ def product_details(request, product_id):
         else:
             return HttpResponseRedirect(reverse('Store:orderapp_details', kwargs={'orderapp_id': order.id}))
     return render(request, 'store/product_details.html', context)
-
